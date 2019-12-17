6b94d44eb8bd8ee05a9e0922675745c224551dcc
kwlahr <kwlahr@gmail.com>
2019-12-16 21:25:09 -0700

    fixed index.js and updated html routes

--- public/js/index.js
+++ public/js/index.js
@@ -3,39 +3,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   // Dropdowns in navbar
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function (example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function () {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function (id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function () {
-  API.getExamples().then(function (data) {
-    var $examples = data.map(function (example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
 
   var $dropdowns = getAll('.navbar-item.has-dropdown:not(.is-hoverable)');
 
@@ -67,10 +34,6 @@ var refreshExamples = function () {
   });
 
   // Toggles
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function (event) {
-  event.preventDefault();
 
   var $burgers = getAll('.burger');
 
@@ -91,26 +54,3 @@ var handleFormSubmit = function (event) {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
   }
 });
-  API.saveExample(example).then(function () {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function () {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function () {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
